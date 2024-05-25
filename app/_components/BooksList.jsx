1414f0740ee46c3a521f9ef43c072bf6e7fa7357
lorraineC26 <lorraine.quinyee@gmail.com>
2024-05-25 02:06:51 -0400

    replace mock data with a real fetch from api/books

--- app/_components/BooksList.jsx
+++ app/_components/BooksList.jsx
@@ -1,26 +1,7 @@
 import { Button } from '@/components/ui/button';
 import React from 'react'
 
-const BooksList = () => {
-  // mock books data
-  const books = [
-    {
-      id:1,
-      title: "The Book of Bill",
-      price: 35.99,
-      category: "fiction",
-      description:
-        "The demon that terrorized Gravity Falls is back from the great beyond to finally tell his side of the story in The Book of Bill, written by none other than Bill Cipher himself. Inside, Bill sheds light on his bizarre origins, his sinister effects on human history, the Pines family’s most embarrassing secrets, and the key to overthrowing the world (laid out in a handy step-by-step guide). This chaotic and beautifully illustrated tome contains baffling riddles, uncrackable ciphers, lost Journal 3 pages, ways to cheat death, the meaning of life, and a whole chapter on Silly Straws. But most importantly, The Book of Bill is deeply, deeply cursed.",
-    },
-    {
-      id:2,
-      title: "Collide",
-      price: 20.0,
-      category: "romance",
-      description:
-        "An ultimatum from Summer Preston’s thesis advisor thrusts her into an unexpected collision with the hockey team’s captain, Aiden Crawford. She’s caught between conflicting desires of fulfilling her lifelong dream of becoming a sport psychologist and staying as far away as possible from the god-awful sport. And once she meets Aiden—well, let’s just say he confirms all her worst assumptions about hockey players.",
-    },
-  ];
+const BooksList = ({books}) => {
 
   return (
     <div className="mt-24 px-4">
--- app/page.js
+++ app/page.js
@@ -1,9 +1,31 @@
+"use client";
+
 import BooksList from "./_components/BooksList";
+import { useEffect, useState } from "react";
 
 export default function Home() {
+  const [books, setBooks] = useState([]);
+
+  // fetch all books data from /api/books
+  useEffect(() => {
+    const fetchBooks = async () => {
+      const res = await fetch("/api/books");
+      const data = await res.json();
+
+      // debugging
+      console.log("books fetched: ", data);
+
+      // set all books data to the books state
+      setBooks(data);
+    };
+
+    fetchBooks();
+  }, [])
+
+
   return (
     <>
-      <BooksList />
-    </> 
+      <BooksList books={books} />
+    </>
   );
 }
