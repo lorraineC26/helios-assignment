b75febcaeecc44111e423b167e50765323b2396b
lorraineC26 <lorraine.quinyee@gmail.com>
2024-05-25 00:38:54 -0400

    clean db before seeding

--- prisma/seed/bookSeedLogic.js
+++ prisma/seed/bookSeedLogic.js
@@ -3,15 +3,25 @@ import books from "./bookSeed.js";
 
 const prisma = new PrismaClient();
 
-// create book records in the Books table
-async function seedBooks() { 
-  for (const book of books) {
-    await prisma.books.create({
-      data: book,
-    });
-    console.log("Books seeded successfully");
+
+const seedBooks = async () => {
+  try {
+    // delete existing book records to prevent duplications
+    await prisma.books.deleteMany();
+
+    // create book records in the Books table
+    for (const book of books) {
+      await prisma.books.create({
+        data: book,
+      });
+    }
+  } catch (error) {
+    console.error("Errors seeding book data into the Books table", error);
+  } finally {
+    // disconnect the client
+    await prisma.$disconnect();
   }
-}
+};
 
 seedBooks();
 
