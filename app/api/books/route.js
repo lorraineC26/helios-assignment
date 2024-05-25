74f37faacc9ff7fde9fb7b716512737fcc8ac5e5
lorraineC26 <lorraine.quinyee@gmail.com>
2024-05-25 18:42:29 -0400

    fix add-new-book data type issue

--- app/api/books/route.js
+++ app/api/books/route.js
@@ -12,8 +12,16 @@ export async function GET(req) {
 // POST handler to create a new book in the db
 export async function POST(req) {
   const newBook = await req.json();
+
+  const { title, price, category, description } = newBook;
+
   const book = await prisma.books.create({
-    data: newBook,
+    data: {
+      title,
+      price: parseFloat(price),
+      category,
+      description,
+    },
   });
 
   return new Response(JSON.stringify(book), { status: 201 });
