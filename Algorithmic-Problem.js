a7e836c738df4fbbfa536cc66a0985156aa58b1b
lorraineC26 <lorraine.quinyee@gmail.com>
2024-05-25 19:44:45 -0400

    upload Algorithmic Problem

---
+++ Algorithmic-Problem.js
@@ -0,0 +1,33 @@
+/*
+ * Problem Statement: Array Manipulation**
+ * Write a function `findMaxConsecutiveOnes` that
+ * takes a binary array `nums`  and
+ * returns the maximum number of consecutive 1s in the array.
+ */
+
+function findMaxConsecutiveOnes(nums) {
+  let match = 0; // keep track the current match
+  let maxMatches = 0; // keep track the maximum matches
+
+  for (let i = 0; i < nums.length; i++) {
+
+    // only count for consecutive 1s
+    if (nums[i] === 1) {
+      match++; 
+
+      // Update the max if current match is greater
+      if (match > maxMatches) {
+        maxMatches = match; 
+      }
+    } else {
+      // Reset to 0 if it's not 1
+      match = 0;
+    }
+  }
+
+  return maxMatches;
+}
+
+// Example usage
+console.log(findMaxConsecutiveOnes([1, 1, 0, 1, 1, 1])); // Output: 3
+console.log(findMaxConsecutiveOnes([1, 0, 1, 1, 0, 1])); // Output: 2
