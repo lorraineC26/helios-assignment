ba2af9818360385af4a19028c656caf825811546
lorraineC26 <lorraine.quinyee@gmail.com>
2024-05-25 01:17:43 -0400

    impl GET endpoint to fetch all books

---
+++ app/api/books/route.js
@@ -0,0 +1,10 @@
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+// GET endpoint to browse all books
+export async function GET(req) {
+  const books = await prisma.books.findMany();
+
+  return new Response(JSON.stringify(books), { status: 200 });
+}
